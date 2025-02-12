a42aee9e3c26b47b5dfe5590921f8b6f95522aa7
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-12 15:41:35 +0700

    เพิ่มการกำหนดค่า build ใน vite config ไฟล์

--- docs-website/vite.config.js
+++ docs-website/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import mdx from '@mdx-js/rollup';
 import RemarkGfm from 'remark-gfm';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -27,4 +28,18 @@ export default defineConfig({
   server: {
     port: 3000
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const moduleName = id.split('node_modules/')[1].split('/')[0];
+            return path.join('chunk', moduleName);
+          }
+
+          return null;
+        }
+      }
+    }
+  }
 });
