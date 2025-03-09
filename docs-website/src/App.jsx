a70afa5adac0611b81296e653a0cf911b43383e3
NekoSakuraLucia <mokurolucia@gmail.com>
2025-03-09 17:36:25 +0700

    ลบคอมโพเนนต์ CodeEditor และ อัปเดต DocsPage

--- docs-website/src/App.jsx
+++ docs-website/src/App.jsx
@@ -5,27 +5,28 @@ import DocsPage from './pages/DocsPage';
 import HomePage from './pages/HomePage';
 import { NotificationProvider } from './contexts/NotificationProvider';
 import { ThemeProvider } from './contexts/Theme/ThemeProvider';
-import CodeEditor from './pages/CodeEdit/CodeEditor';
 
 function App() {
-  return (
-    <ThemeProvider>
-      <NotificationProvider>
-        <BrowserRouter>
-          <NextTopLoader showSpinner={false} color='#a855f7' />
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="docs" element={<DocsPage />}>
-                <Route path=":category/:slug" element={<DocsPage />} />
-              </Route>
-              <Route path='edit/:slug' element={<CodeEditor />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </NotificationProvider>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <NotificationProvider>
+                <BrowserRouter>
+                    <NextTopLoader showSpinner={false} color="#a855f7" />
+                    <Routes>
+                        <Route path="/" element={<Layout />}>
+                            <Route index element={<HomePage />} />
+                            <Route path="docs" element={<DocsPage />}>
+                                <Route
+                                    path=":category/:slug"
+                                    element={<DocsPage />}
+                                />
+                            </Route>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </NotificationProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
